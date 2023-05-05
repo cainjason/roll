f6e663fd6f01e588ace0955bb53be656b69bf521
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-04 22:38:09 -0400

    chore: Removed unwanted files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Roll() {
+    return <h1> Position, position and position </h1>
+}
+
+ReactDom.render(<Roll />, document.getElementById('root'));

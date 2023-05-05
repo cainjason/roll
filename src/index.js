b01b7616b1f7a824a2cba96594b97caae04840b0
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-05 00:07:38 -0400

    chore: Created an index.css file with a reset

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import ReactPlayer from "react-player";
+import './components/index.css';
+
+
+
+
 
 function Roll() {
-    return <h1> Position, position and position </h1>
+    return (
+        <>
+            <h1> Position, position and position </h1>
+            <ReactPlayer id='section' url='https://fb.watch/k5blMm1Ac9/' />
+        </>
+    );
+
 }
 
 ReactDom.render(<Roll />, document.getElementById('root'));
